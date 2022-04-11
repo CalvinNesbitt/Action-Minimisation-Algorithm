258c7b50556f52fb81c8e8eb673ff46ce5e0fb1d
Calvin Nesbitt <c.f.nesbitt@student.reading.ac.uk>
2022-04-11 15:53:00 +0100

    Updated processor count limit for cx1 limtations

--- fw_action_fps.py
+++ fw_action_fps.py
@@ -59,7 +59,7 @@ class FreidlinWentzell:
     ## Need to used different differencing at end points
     ########################################################
 
-    # Forward differencing for initial velocity
+    # Forward differencing for first 2 velocities
     # x should be shape (2, ndim)
 
     def _v0(self, x, dt, p):
@@ -69,17 +69,18 @@ class FreidlinWentzell:
         v = self._v0(x, dt, p)
         return  v @ self._diff_inv(x[0], p) @ v
 
-    # Cenetered differencing away from the boundary points
-    # x should be shape (3, ndim)
+    # Five point stencil away from the boundary points
+    # x should be shape (5, ndim)
 
     def _vk(self, x, dt, p):
-        return (x[2] - x[0])/(2*dt) - self._b(x[1], p)
+        _phi_k_dot = (x[0] - 8*x[1] + 8*x[3] - x[4])/12*dt
+        return _phi_k_dot - self._b(x[2], p)
 
     def _Lk(self, x, dt, p):
         v = self._vk(x, dt, p)
-        return v @ self._diff_inv(x[1], p) @ v
+        return v @ self._diff_inv(x[2], p) @ v
 
-    # Backward differencing for final velocity
+    # Backward differencing for final two velocities
     # x should be shape (2, ndim)
 
     def _vN(self, x, dt, p):
@@ -103,15 +104,7 @@ class FreidlinWentzell:
         p: list
             Arguments for drift and diffusion functions.
         """
-        Ls = np.empty(path.shape[0])
-
-        #Boundary Values
-        Ls[0] = self._L0(path[:2], time[1] - time[0], p)
-        Ls[-1] = self._LN(path[-2:], time[-1] - time[-2], p)
-
-        # Middle Values
-        for i in range(1, path.shape[0] - 1):
-            Ls[i] = self._Lk(path[i-1:i+2], time[i+1] - time[i], p)
+        Ls = self.lagrangians(path, time, p)
         return 0.5 * np.trapz(Ls, dx=time[1] - time[0])
 
     def lagrangians(self, path, time, p):
@@ -132,11 +125,13 @@ class FreidlinWentzell:
 
         #Boundary Values
         Ls[0] = self._L0(path[:2], time[1] - time[0], p)
+        Ls[1] = self._L0(path[1:3], time[2] - time[1], p)
+        Ls[-2] = self._LN(path[-3:-1], time[-2] - time[-3], p)
         Ls[-1] = self._LN(path[-2:], time[-1] - time[-2], p)
 
         # Middle Values
-        for i in range(1, path.shape[0] - 1):
-            Ls[i] = self._Lk(path[i-1:i+2], time[i+1] - time[i], p)
+        for i in range(2, path.shape[0] - 2):
+            Ls[i] = self._Lk(path[i-2:i+3], time[i+1] - time[i], p)
         return Ls
 
 
@@ -191,19 +186,42 @@ class JFW(FreidlinWentzell):
         grad_Ls = np.empty(path.shape)
 
         # Boundary Values - dt/2 from trapz rule
-        grad_Ls[0] = dt/2 * self._grad_L0(path[:2], dt, p)[0] + dt * self._grad_Lk(path[:3], dt, p)[0]
-        grad_Ls[1] = dt/2 * self._grad_L0(path[:2], dt, p)[1] + dt * (self._grad_Lk(path[:3], dt, p)[1] + self._grad_Lk(path[1:4], dt, p)[0])
+        #dL/d_phi_0
+        grad_Ls[0] = dt/2 * self._grad_L0(path[:2], dt, p)[0] + dt * self._grad_Lk(path[:5], dt, p)[0]
 
-        grad_Ls[-2] =(
-            dt/2 * self._grad_LN(path[-2:], dt, p)[0] + dt * (self._grad_Lk(path[-3:], dt, p)[1] + self._grad_Lk(path[-4:-1], dt, p)[2])
+        #dL/d_phi_1
+        grad_Ls[1] = dt/2 * self._grad_L0(path[:2], dt, p)[1] + dt * (self._grad_L0(path[1:3], dt, p)[0] + self._grad_Lk(path[:5], dt, p)[1] + self._grad_Lk(path[1:6], dt, p)[0])
+
+        #dL/d_phi_2
+        grad_Ls[2] = dt * (
+        self._grad_L0(path[1:3], dt, p)[1] + self._grad_Lk(path[:5], dt, p)[2]  + self._grad_Lk(path[1:6], dt, p)[1] + self._grad_Lk(path[2:7], dt, p)[0]
         )
-        grad_Ls[-1] = dt/2 * self._grad_LN(path[-2:], dt, p)[1] + dt * self._grad_Lk(path[-3:], dt, p)[2]
 
-        # Computing grad_Lk[phi_k] in parallel
-        num_cores = 8#multiprocessing.cpu_count()
-        dv_list = Parallel(n_jobs=num_cores)(delayed(self._grad_Lk)(path[i-2:i+1], dt, p) for i in range(2, path.shape[0]))
+        #dL/d_phi_3
+        grad_Ls[3] = dt * (
+        self._grad_Lk(path[:5], dt, p)[3]  + self._grad_Lk(path[1:6], dt, p)[2] + self._grad_Lk(path[2:7], dt, p)[1] + self._grad_Lk(path[3:8], dt, p)[0]
+        )
 
-        # Summing the three terms in each grad_L derivative
-        for i in range(len(dv_list[:-2])):
-            grad_Ls[i+2] = dt * (dv_list[i][2] + dv_list[i+1][1] + dv_list[i+2][0])
+        # Non Boundary Terms
+
+        # Computing dL/d_phi_k in parallel
+        num_cores = 32#multiprocessing.cpu_count()
+        dv_list = Parallel(n_jobs=num_cores)(delayed(self._grad_Lk)(path[i-4:i+1], dt, p) for i in range(4, path.shape[0]))
+
+        # Summing the four terms in each dL/d_phi_k
+        for i in range(len(dv_list[:-4])):
+            grad_Ls[i + 4] = dt * (dv_list[i][4] + dv_list[i+1][3] + dv_list[i+2][2] + dv_list[i+3][1] + dv_list[i+4][0])
+
+
+        grad_Ls[-4] =(
+            dt * (self._grad_Lk(path[-5:], dt, p)[1] + self._grad_Lk(path[-6:-1], dt, p)[2] + self._grad_Lk(path[-7:-2], dt, p)[3] + self._grad_Lk(path[-8:-3], dt, p)[4])
+        )
+        grad_Ls[-3] =(
+            dt * (self._grad_Lk(path[-5:], dt, p)[2] + self._grad_Lk(path[-6:-1], dt, p)[3] + self._grad_Lk(path[-7:-2], dt, p)[4] + self._grad_LN(path[-3:-1], dt, p)[0])
+        )
+        grad_Ls[-2] =(
+            dt * (self._grad_Lk(path[-5:], dt, p)[3] + self._grad_Lk(path[-6:-1], dt, p)[4] + self._grad_LN(path[-3:-1], dt, p)[1]
+        ) + dt/2 * self._grad_LN(path[-2:], dt, p)[0]
+        )
+        grad_Ls[-1] = dt * self._grad_Lk(path[-5:], dt, p)[4] + dt/2 * self._grad_LN(path[-2:], dt, p)[1]
         return 0.5 * grad_Ls
